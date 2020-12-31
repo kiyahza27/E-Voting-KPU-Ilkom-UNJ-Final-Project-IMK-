7daf149a8d84ab19d9699296576c2bed8c4ddf97
Novitasari <59106885+novitasari353@users.noreply.github.com>
2020-12-31 14:58:57 +0700

    Add files via upload

---
+++ pemilih/calon/pilih_calon3.php
@@ -0,0 +1,30 @@
+<?php
+$data_username = $_SESSION["ses_username"];
+
+    if(isset($_GET['kode'])){
+		$sql_simpan = "INSERT INTO tb_vote3 (id_calon, id_pemilih) VALUES (
+            '".$_GET['kode']."',
+            '".$data_username."');";
+        $sql_simpan .= "UPDATE tb_pengguna set 
+			status3='0'
+			WHERE username='".$data_username."'";
+        $query_simpan = mysqli_multi_query($koneksi, $sql_simpan);
+		mysqli_close($koneksi);
+		
+		if ($query_simpan) {
+			echo "<script>
+			Swal.fire({title: 'Anda Berhasil Memilih',text: '',icon: 'success',confirmButtonText: 'OK'
+			}).then((result) => {if (result.value){
+				window.location = 'index.php?page=bilik-suara3';
+				}
+			})</script>";
+			}else{
+			echo "<script>
+			Swal.fire({title: 'Anda Gagal Memilih',text: '',icon: 'error',confirmButtonText: 'OK'
+			}).then((result) => {if (result.value){
+				window.location = 'index.php?page=bilik-suara3';
+				}
+			})</script>";
+		  }}
+		   //selesai proses simpan data
+	  
